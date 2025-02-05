ee5f78df0e8c06a1a7e0d18d967c33556822e24d
Operator <039kirill@gmail.com>
2025-02-05 03:40:30 +0200

    Add files via upload
    With APK

--- DemoEx/src/main/java/data/DatabaseConfig.java
+++ DemoEx/src/main/java/data/DatabaseConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/production_management";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "3318493145Lol";
+    private static final String DB_PASSWORD = "3318493145";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
